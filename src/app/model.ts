e7dd875ab2d11990425767244e371cd9bdb99e5c
Adam Cross <adamfgcross@gmail.com>
2021-10-06 14:28:21 -0400

    add model file

---
+++ src/app/model.ts
@@ -0,0 +1,12 @@
+var model = {
+    user: "Adam",
+    items: [{
+        action : "Buy Flowers",
+        done : false
+    },
+    {  
+        action: "Get Shoes",
+        done : false
+    }
+    ]
+}
